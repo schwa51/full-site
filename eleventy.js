c1d415cac2e11bcb18dc97601b3aee6d3c098975
schwa51 <samuel.vaughn@gmail.com>
2025-05-02 22:57:11 -0700

    Add files via upload

---
+++ eleventy.js
@@ -0,0 +1,31 @@
+module.exports = function (eleventyConfig) {
+    // All NPCs
+    eleventyConfig.addCollection("npcs", (collection) =>
+      collection.getFilteredByGlob("npcs/*.md")
+    );
+  
+    // Only public NPCs
+    eleventyConfig.addCollection("npcs_public", (collection) =>
+      collection.getFilteredByGlob("npcs/*.md").filter((item) => item.data.public)
+    );
+  
+    // Sessions, Items, Lore
+    eleventyConfig.addCollection("sessions", (collection) =>
+      collection.getFilteredByGlob("sessions/*.md")
+    );
+    eleventyConfig.addCollection("items", (collection) =>
+      collection.getFilteredByGlob("items/*.md")
+    );
+    eleventyConfig.addCollection("lore", (collection) =>
+      collection.getFilteredByGlob("lore/*.md")
+    );
+  
+    return {
+      dir: {
+        input: ".",
+        includes: "_includes",
+        output: "_site",
+      },
+    };
+  };
+  
