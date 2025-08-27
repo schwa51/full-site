f35d58fe5fe061d1be49b58ba022d9196a7615df
schwa51 <samuel.vaughn@gmail.com>
2025-08-26 22:05:36 -0400

    Dolmenwood

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,7 @@ module.exports = function (eleventyConfig) {
       "wildsea": "vault/campaigns/The Wildsea campaign",
       "timewatch": "vault/campaigns/Timewatch campaign",
       "mythic": "vault/campaigns/Mythic Bastionland campaign"
+      "dolmenwood": "vault/campains/Dolmenwood"
       // add more as needed
     };
   
