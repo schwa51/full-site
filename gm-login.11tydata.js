aa0005ff49eec57be69b8e0f2a489bba409b877e
schwa51 <samuel.vaughn@gmail.com>
2025-10-06 19:31:37 -0400

    converted to ESM

--- gm-login.11tydata.js
+++ gm-login.11tydata.js
@@ -1,8 +1,8 @@
 // gm-login.11tydata.js
-module.exports = {
+export default {
   title: "GM Login",
   gm: true,
   layout: null,
-  // Only emit in GM build
   permalink: () => (process.env.GM_MODE ? "/login/" : false),
 };
+
