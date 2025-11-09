6536078324ac65e9695e1e9c0c5cf302162ef540
schwa51 <samuel.vaughn@gmail.com>
2025-11-09 17:59:56 -0500

    one more add

--- eleventy.config.js
+++ eleventy.config.js
@@ -265,6 +265,17 @@ eleventyConfig.addCollection("nav_content", (api) =>
   // ------------------------------
   eleventyConfig.addGlobalData("eleventyComputed", {
     permalink: (data) => {
+      const parsed = parsePath(d);
+const campaignSlug = safe(d.campaign || parsed.campaignSeg);
+
+console.log("[PERMALINK DEBUG]", {
+  inputPath: d.page?.inputPath,
+  filePathStem: d.page?.filePathStem,
+  parsed,                   // { stem, campaignSeg, sectionSeg, isIndex }
+  fm_campaign: d.campaign,
+  resolved_campaignSlug: campaignSlug,
+});
+
       if (data.permalink !== undefined) return data.permalink;
 
       const inputPath = String(data.page?.inputPath || "").replace(/\\/g, "/");
